dff8ea9c9f108ce3dfe5ef897543c58081c3d7d6
Dustin Gogoll <dustin.gogoll@googlemail.com>
2019-11-30 20:06:47 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import * as Express from 'express';
+import { Express as IExpress } from 'express';
+import bodyParser = require('body-parser');
+import cors = require('cors');
+import Routes from './api/routes';
+
+const port: number = Number(process.env.PORT) || 3000; // port / default port
+
+// Start up an instance of app
+const app: IExpress = Express();
+
+// Configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Register all routes
+Routes(app);
+
+app.listen(port);
